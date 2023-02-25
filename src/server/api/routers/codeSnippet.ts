23e4f4c792441c3e6d91145e7364be9871ed3f45
TheCoderMG <zyborglp@outlook.com>
2023-02-25 15:47:49 +0100

    basic create form

---
+++ src/server/api/routers/codeSnippet.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+// TODO: Error handling
+export const codeSnippetRouter = createTRPCRouter({
+  create: publicProcedure
+    .input(z.object({ content: z.string().max(4096) }))
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.codeSnippet.create({
+        data: input,
+        select: { id: true },
+      });
+    }),
+  get: publicProcedure
+    .input(z.object({ id: z.string().cuid() }))
+    .query(({ input, ctx }) => {
+      return ctx.prisma.codeSnippet.findUnique({
+        where: { id: input.id },
+        select: { content: true },
+      });
+    }),
+});
